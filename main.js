0a8ff86145cf445e04f08eaa6640bbfdb10a261c
Maxim Demidov <maaksaa@gmail.com>
2020-03-03 23:26:41 +0300

    Добавил дублирование строки по кнопке "+" Задание 1: Выполнено.

--- main.js
+++ main.js
@@ -1,46 +1,44 @@
 var i = 1;
 
 function test() {
-    var elementSearch = document.getElementById("content2");
-    
-
-    // добавляю перенос строки
-    var addLineBreak = document.createElement('br');
-    elementSearch.appendChild(addLineBreak);
-
-    // первое поле для фио
-    var copyText = document.getElementById("myInputFio");
-    copyText.select();
-    document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
-    var inputFio = document.createElement('input');
-    inputFio.setAttribute("type", "text");
-    inputFio.setAttribute("name", i);
-    inputFio.setAttribute("value", copyText.value);
-    elementSearch.appendChild(inputFio);
-    
-
-    i++;
-    alert(i);
-    alert('123');
+  var elementSearch = document.getElementById("content2");
 
 
-}
+  // добавляю перенос строки
+  var addLineBreak = document.createElement('br');
+  elementSearch.appendChild(addLineBreak);
 
-function test2() {
-    var elementSearch = document.getElementById("content2");
+  // копирую значение из input где фио
+  var copyTextFio = document.getElementById("myInputFio").value;
+  // copyTextFio.select();
+  // document.execCommand("copy");
+  // alert("Copied the text: " + copyTextFio);
 
-      // третья форма с телефоном
-      document.getElementById("MyInputTelNumber").innerHTML = form.telephonNumber.value;
-      var copyText = document.getElementById("myInputFio");
-      copyText.select();
-      document.execCommand("copy");
+  // копирую значение из input с телефоном
+  var copyTextNumber = document.getElementById('myInputTextNumber').value;
+  // copyTextNumber.select();
+  // document.execCommand("copy");
+  // alert("Copied the text: " + copyTextNumber);
 
-      
-      alert("Copied the text: " + copyText.value);
-      var inputFio = document.createElement('input');
-      inputFio.setAttribute("type", "tel");
-      inputFio.setAttribute("name", i);
-      inputFio.setAttribute("value", copyText.value);
-      elementSearch.appendChild(inputFio);
+
+  // создаю инпут с фио и добавляю значение
+  var inputFio = document.createElement('input');
+  inputFio.setAttribute("type", "text");
+  inputFio.setAttribute("name", i);
+  inputFio.setAttribute("value", copyTextFio);
+  elementSearch.appendChild(inputFio);
+
+  // создаю инпут с телефоном и добавляю значение
+  var inputNumber = document.createElement('input');
+  inputNumber.setAttribute("type", "text");
+  inputNumber.setAttribute("name", i);
+  inputNumber.setAttribute("value", copyTextNumber);
+  elementSearch.appendChild(inputNumber);
+
+
+  i++;
+  alert(i);
+
+
+
 }
