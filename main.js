818cb37cd8bc71e92623913912d61622b3156d41
Maxim Demidov <maaksaa@gmail.com>
2020-03-04 17:09:53 +0300

    Добавлено: При выборе опции в селект, формируется строка с рандомным числом

--- main.js
+++ main.js
@@ -3,23 +3,15 @@ var i = 1;
 function test() {
   var elementSearch = document.getElementById("content2");
 
-
   // добавляю перенос строки
   var addLineBreak = document.createElement('br');
   elementSearch.appendChild(addLineBreak);
 
   // копирую значение из input где фио
   var copyTextFio = document.getElementById("myInputFio").value;
-  // copyTextFio.select();
-  // document.execCommand("copy");
-  // alert("Copied the text: " + copyTextFio);
 
   // копирую значение из input с телефоном
   var copyTextNumber = document.getElementById('myInputTextNumber').value;
-  // copyTextNumber.select();
-  // document.execCommand("copy");
-  // alert("Copied the text: " + copyTextNumber);
-
 
   // создаю инпут с фио и добавляю значение
   var inputFio = document.createElement('input');
@@ -35,10 +27,19 @@ function test() {
   inputNumber.setAttribute("value", copyTextNumber);
   elementSearch.appendChild(inputNumber);
 
-
   i++;
   alert(i);
 
+}
 
+function randomInfo() {
+  let div = document.createElement('div');
+  div.className = "alert";
+  div.innerHTML = "<strong>Всем привет!</strong>." + getRandomNumber(255);
+  document.body.append(div);
 
+}
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
